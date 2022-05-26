d69cbf4fe212c82cbcbf3a8719017c8b841bb113
German Smurov <smurovnemez@mail.ru>
2022-05-26 22:50:50 +0300

    Practice algorithms-v2

---
+++ binary2.js
@@ -0,0 +1,25 @@
+const bin = (arr, val) => {
+    let mid = Math.floor(arr.length / 2);
+    const maxVal = arr[arr.length - 1];
+
+    if (val > maxVal) {
+        return -1;
+    }
+
+    while (mid >= 0 && mid <= arr.length) {
+        if (arr[mid] === val) {
+            return arr[mid]
+        }
+
+        if (arr[mid] > val) {
+            mid = Math.floor(mid / 2)
+        }
+
+        if (arr[mid] < val) {
+            mid = Math.floor((arr.length - mid) / 2)
+        }
+    }
+    return -1;
+}
+
+console.log(bin([1, 2, 3, 4, 5, 6, 7, 8]))
---
+++ bubble.js
@@ -0,0 +1,18 @@
+//bubble 
+
+const bubble = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+
+    return arr;
+}
+
+console.log(bubble([3, 2, 7, 56, 2, 1]))
